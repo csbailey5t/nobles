101700ed8706600a976f67902d0c0faaa8fde63f
Scott Bailey <csb5t@virginia.edu>
2016-03-29 14:19:40 -0400

    Build the corpus and get feature names

--- classify.py
+++ classify.py
@@ -4,12 +4,21 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 stemmer = SnowballStemmer('english')
 stopset = set(stopwords.words('english'))
 
+dataFile = 'AshleyBillasanoTweets.csv'
 
-# Read in the data
-tweets = pd.read_csv('AshleyBillasanoTweets.csv', encoding='latin-1')
+
+def build_corpus_from_csv(filename, column):
+    corpus = []
+    data = pd.read_csv(filename, encoding='latin-1', index_col=0)
+    for entry in data[column]:
+        corpus.append(entry)
+
+    return corpus
 
 
 def tokenizer(tweet, stopset):
@@ -19,16 +28,35 @@ def tokenizer(tweet, stopset):
     # remove stopwords
     tokens = [token for token in tokens if token not in stopset]
     words = [
-        stemmer.stem(word)
+        # stemmer.stem(word)
+        word
         for word in tokens
         if word.isalnum()
     ]
     return words
 
 
+def create_vectorizer(corpus):
+    vectorizer = TfidfVectorizer(input='content', stop_words='english')
+    vectorizer.fit_transform(corpus)
+
+    return vectorizer
+
+
+def get_features(corpus):
+    vectorizer = create_vectorizer(corpus)
+    features = vectorizer.get_feature_names()
+
+    return features
+
+
 def main():
-    tokenTweet = tokenizer(tweets['Tweet'][0], stopset)
-    print(tokenTweet)
+
+    corpus = build_corpus_from_csv(dataFile, 'Tweet')
+
+    tweet_feats = get_features(corpus)
+
+    print(tweet_feats)
 
 if __name__ == '__main__':
     main()
