7c7a51ea4ae0416893328f576373787e7c9969f8
Scott Bailey <csb5t@virginia.edu>
2016-06-30 15:56:39 -0400

    Plan ahead; add in static data type for instances

--- convert_data.py
+++ convert_data.py
@@ -21,6 +21,32 @@ def split_datetime(df):
     return texts
 
 
+def fill_in_SMS_data(df):
+    data_type = 'SMS'
+    # I assume that in_out is whether the participant wrote the message
+    # or received it?
+    # If so, if each participant has their own csv file, it would always be out
+    # If not, if data of all participants in one file, will need to determine
+    # this.
+    # How much do they care about messages received by the individual?
+    in_out = []
+    contact_name = []
+    contact_id = []
+    ind_grp = []
+
+    for index, row in df.iterrows():
+        df.loc[index, 'data type'] = data_type
+        # print(row['data type'])
+        # for contact name, if only two particpats in convo, can just get
+        # other key
+        # if using pandas groupby
+        # for contact id, will need to get from reference sheet at some pt
+        # for ind_group, if only two, indv? if more than two keys, grp?
+
+        # if do groupby, count keys to see if grp or ind
+    return df
+
+
 def main():
     # while the file format for the iphone SMS data is csv,
     # it's actually a tsv file
@@ -30,12 +56,16 @@ def main():
     )
     # split date time
     texts = split_datetime(text_conversation)
-
+    # define headers for data format
+    # headers = ['participant id', 'data type', 'date',
+            #    'time', 'in_out', 'contact name', 'contact id',
+            #    'ind_grp', 'message']
+    # reorder columns in texts df to match headers
+    # cols = ['user', 'data type', 'date', 'time', 'in_out', 'contact_name',
+            # 'contact id', 'ind_grp', 'message']
+    texts = fill_in_SMS_data(texts)
     print(texts)
-    # grouped = text_conversation.groupby(['user'])
-    # split into two dataframes, based on user.
 
-    # should be able to use pd.groupby
 
 if __name__ == '__main__':
     main()
