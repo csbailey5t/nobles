11c08a101a3336a8000c8102370eaceb5951eecf
Scott Bailey <csb5t@virginia.edu>
2016-07-05 11:42:18 -0400

    Refactor to make it classy

--- convert_data.py
+++ convert_data.py
@@ -7,75 +7,87 @@ import pandas as pd
 CONVO = 'sample_text_convo.csv'
 
 
-def split_datetime(df):
-    date_time = df['datetime']
-    date = []
-    time = []
-    for timestamp in date_time:
-        pieces = timestamp.split(' ')
-        date.append(pieces[0])
-        time.append(pieces[1])
-    texts = df.drop('datetime', 1)
-    texts['date'] = pd.Series(date)
-    texts['time'] = pd.Series(time)
-    return texts
+class SMSTransformer:
+    """Takes a tsv file of SMS messages, manipulates them,
+     and reformates them."""
 
+    def __init__(self, data):
+        self.data = data
+        self.df = None
+        self.texts = None
+        self.full_data = None
 
-def fill_in_SMS_data(df):
-    data_type = 'SMS'
-    # I assume that in_out is whether the participant wrote the message
-    # or received it?
-    # If so, if each participant has their own csv file, it would always be out
-    # If not, if data of all participants in one file, will need to determine
-    in_out = "out"
-    # this.
-    # How much do they care about messages received by the individual?
-    contact_name = []
-    # for the moment, let's set contact id to "none"
-    # we'll have to grab this from some master reference?
-    contact_id = 'none'
+        self.read_data()
+        self.split_datetime()
+        self.fill_in_SMS_data()
 
-    users = df['user']
-    users = users.tolist()
-    users = set(users)
-    num_users = len(users)
-    if num_users > 2:
-        ind_grp = "grp"
-    else:
-        ind_grp = "ind"
+    def __repr__(self):
+        return "{}".format(self.full_data)
 
-    for index, row in df.iterrows():
-        user_list = list(users)
-        user_name = row['user']
-        contact_name = user_list[1] if user_name == user_list[0] \
-            else user_list[0]
+    def read_data(self):
+        df = pd.read_table(
+            self.data, encoding='latin-1', sep='\t',
+            names=['user', 'phone number', 'datetime', 'message']
+        )
+        self.df = df
 
-        df.loc[index, 'data type'] = data_type
-        df.loc[index, 'in_out'] = in_out
-        df.loc[index, 'contact name'] = contact_name
-        df.loc[index, 'contact id'] = 'none'
-        df.loc[index, 'ind_grp'] = ind_grp
+    def split_datetime(self):
+        date_time = self.df['datetime']
+        date = []
+        time = []
+        for timestamp in date_time:
+            pieces = timestamp.split(' ')
+            date.append(pieces[0])
+            time.append(pieces[1])
+        texts = self.df.drop('datetime', 1)
+        texts['date'] = pd.Series(date)
+        texts['time'] = pd.Series(time)
+        self.texts = texts
 
-    return df
+    def fill_in_SMS_data(self):
+        data_type = 'SMS'
+        # I assume that in_out is whether the participant wrote the message
+        # or received it?
+        # If each participant has their own csv file, it would always be out
+        # If not, if data of all participants in one file,
+        # will need to determine
+        in_out = "out"
+        # this.
+        # How much do they care about messages received by the individual?
+        contact_name = []
+        # for the moment, let's set contact id to "none"
+        # we'll have to grab this from some master reference?
+        contact_id = 'none'
+        df = self.texts
+
+        users = df['user']
+        users = users.tolist()
+        users = set(users)
+        num_users = len(users)
+        if num_users > 2:
+            ind_grp = "grp"
+        else:
+            ind_grp = "ind"
+
+        for index, row in df.iterrows():
+            user_list = list(users)
+            user_name = row['user']
+            contact_name = user_list[1] if user_name == user_list[0] \
+                else user_list[0]
+
+            df.loc[index, 'data type'] = data_type
+            df.loc[index, 'in_out'] = in_out
+            df.loc[index, 'contact name'] = contact_name
+            df.loc[index, 'contact id'] = contact_id
+            df.loc[index, 'ind_grp'] = ind_grp
+
+        self.full_data = df
 
 
 def main():
     # while the file format for the iphone SMS data is csv,
     # it's actually a tsv file
-    text_conversation = pd.read_table(
-        CONVO, encoding='latin-1', sep='\t',
-        names=['user', 'phone number', 'datetime', 'message']
-    )
-    # split date time
-    texts = split_datetime(text_conversation)
-    # define headers for data format
-    # headers = ['participant id', 'data type', 'date',
-            #    'time', 'in_out', 'contact name', 'contact id',
-            #    'ind_grp', 'message']
-    # reorder columns in texts df to match headers
-    # cols = ['user', 'data type', 'date', 'time', 'in_out', 'contact_name',
-            # 'contact id', 'ind_grp', 'message']
-    texts = fill_in_SMS_data(texts)
+    texts = SMSTransformer(CONVO)
     print(texts)
 
 
